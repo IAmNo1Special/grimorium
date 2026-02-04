26292b90e5e37c11f9a977af781679de3697c75c
Malcom Godlike <ivmno1special@gmail.com>
2026-02-04 01:43:45 -0500

    added: "GRIMORIUMS_INDEX_NAME" constant

--- src/magetools/constants.py
+++ src/magetools/constants.py
@@ -3,4 +3,5 @@
 MAGETOOLS_DIR_NAME = ".magetools"
 DB_FOLDER_NAME = ".chroma_db"
 STANDARD_COLLECTION_NAME = "spells"
+GRIMORIUMS_INDEX_NAME = "grimoriums_index"
 COLLECTION_ATTR_NAME = "__magetools_collection__"
